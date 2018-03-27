8316cd92226a6ad3f894502da40c9a3fca07f1d8
黄巍 <huan1043269994@gmail.com>
2018-03-27 11:28:37 +0800

    fix initGame bug

--- src/config/api.js
+++ src/config/api.js
@@ -3,7 +3,9 @@
  *  
  */ 
 
-const nodeBase = 'http://powerformer.com:3000';
+// const nodeBase = 'http://powerformer.com:3000';
+const nodeBase = 'http://localhost:4000';
+
 
  const questionApi = (id) => ({
    getSingleOption: `/questions/single/${id}/`,
